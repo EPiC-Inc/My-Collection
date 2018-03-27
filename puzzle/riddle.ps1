ec4e513c4df506af09994a4c7fd9ffd89a965872
[R3dact3d] <emmettwb@gmail.com>
2018-03-27 11:09:13 -0600

    Update riddle.ps1

--- puzzle/riddle.ps1
+++ puzzle/riddle.ps1
@@ -1,4 +1,4 @@
-$x = Read-Host -Prompt What do you always bring with you but always leave behind?
+$x = Read-Host -Prompt 'What do you always bring with you but always leave behind?'
 $Bytes = [System.Text.Encoding]::Unicode.GetBytes($x)
 $EncodedText =[Convert]::ToBase64String($Bytes)
 If ($EncodedText -eq 'ZgBpAG4AZwBlAHIAcAByAGkAbgB0AHMA') {
