2cf66b3aa75c8ac88210639309aa56aea53b9406
[R3dact3d] <emmettwb@gmail.com>
2018-03-27 11:25:02 -0600

    Update riddle.ps1

--- puzzle/riddle.ps1
+++ puzzle/riddle.ps1
@@ -1,5 +1,4 @@
 $x = Read-Host -Prompt 'What do you always bring with you but always leave behind?'
-$ans = wget http://bit.ly/2G9mvAq | select-object -expandproperty content
-If ($x -eq $ans) {
-  echo The ip is na
+If ($x -eq 'fingerprints') {
+  echo 'The ip is http://107.2.141.209:8080'
 }
