9513cfdf6bb495b304e70ab5c88f0353303274a4
[R3dact3d] <emmettwb@gmail.com>
2018-03-27 11:16:03 -0600

    Update riddle.ps1

--- puzzle/riddle.ps1
+++ puzzle/riddle.ps1
@@ -1,8 +1,5 @@
 $x = Read-Host -Prompt 'What do you always bring with you but always leave behind?'
-$Bytes = [System.Text.Encoding]::Unicode.GetBytes($x)
-$EncodedText =[Convert]::ToBase64String($Bytes)
-If ($EncodedText -eq 'ZgBpAG4AZwBlAHIAcAByAGkAbgB0AHMA') {
-  $EncodedText = 'aAB0AHQAcAA6AC8ALwAxADAANwAuADIALgAxADQAMQAuADIAMAA5ADoAOAAwADgAMAA='
-  $key = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($EncodedText))
-  echo The password is $key
+$ans = wget http://bit.ly/2G9mvAq | select-object -expandproperty content
+If ($x -eq $ans) {
+  echo The ip is na
 }
