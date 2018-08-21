ff21677184f544d854946ac0cc17b7514ffe4740
[R3dact3d] <emmettwb@gmail.com>
2018-08-21 09:03:33 -0600

    Update llama.ps1

--- benign/llama.ps1
+++ benign/llama.ps1
@@ -1,5 +1,5 @@
 $speaker = New-Object -ComObject SAPI.SPVoice;
 $speaker.Speak("This is only for shits and giggles, provided by Epic Entertainment. No, not really. Here goes!");
 while ($true) {
-  $speaker.Speak("I'M A LAMA");
+  $speaker.Speak("I'M A LAMA");# Supposed to be 'llama', but oh well... stupid TTS
 }
